529e83ea53da322748c18420ad38ceacbc8e7d4c
Shemil Hashan <shemil123@gmail.com>
2023-03-30 02:43:35 -0600

    axios get movie data

--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,18 +1,32 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import axios from 'axios';
 interface Props {
-    navigation: any;
+  navigation: any;
 }
-function DashboardScreen({ navigation }:Props) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Dashboard</Text>
-        <Button
-          title="Favorites"
-          onPress={() => navigation.navigate('Favorites')}
-        />
-      </View>
-    );
-  }
+function DashboardScreen({navigation}: Props) {
+  const [movieData, setMovieData] = useState<any>([]);
+  const [searchText, setSearchText] = useState<string>('mat');
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(
+        `http://www.omdbapi.com/?apikey=62087cc1&s=${searchText}`,
+      );
+      setMovieData(data.data);
+    };
+    if (searchText.length > 2) {
+      getData();
+    }
+  }, [searchText]);
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Dashboard</Text>
+      <Button
+        title="Favorites"
+        onPress={() => navigation.navigate('Favorites')}
+      />
+    </View>
+  );
+}
 
 export default DashboardScreen;
